08ef0983492d65ab2584fefbb8c83507ae619ae6
Buddika Kasun <buddikakasun80@gmail.com>
2024-08-26 17:40:22 +0530

    Modify

--- signUpFrm.Designer.cs
+++ signUpFrm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(signUpFrm));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -35,15 +36,18 @@
             this.pwTextBox = new System.Windows.Forms.TextBox();
             this.signBtn = new System.Windows.Forms.Button();
             this.pwShowCheckBox = new System.Windows.Forms.CheckBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.cancelBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(61, 72);
+            this.label1.Location = new System.Drawing.Point(591, 96);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(118, 32);
+            this.label1.Size = new System.Drawing.Size(134, 38);
             this.label1.TabIndex = 0;
             this.label1.Text = "Sign up";
             // 
@@ -51,9 +55,9 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(63, 147);
+            this.label2.Location = new System.Drawing.Point(437, 209);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(118, 22);
+            this.label2.Size = new System.Drawing.Size(142, 26);
             this.label2.TabIndex = 1;
             this.label2.Text = "User name: ";
             // 
@@ -61,26 +65,28 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(63, 187);
+            this.label3.Location = new System.Drawing.Point(436, 287);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(109, 22);
+            this.label3.Size = new System.Drawing.Size(130, 26);
             this.label3.TabIndex = 2;
             this.label3.Text = "Password: ";
             // 
             // usernameTextBox
             // 
             this.usernameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.usernameTextBox.Location = new System.Drawing.Point(200, 147);
+            this.usernameTextBox.Location = new System.Drawing.Point(591, 209);
+            this.usernameTextBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.usernameTextBox.Name = "usernameTextBox";
-            this.usernameTextBox.Size = new System.Drawing.Size(222, 28);
+            this.usernameTextBox.Size = new System.Drawing.Size(249, 32);
             this.usernameTextBox.TabIndex = 3;
             // 
             // pwTextBox
             // 
             this.pwTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.pwTextBox.Location = new System.Drawing.Point(200, 187);
+            this.pwTextBox.Location = new System.Drawing.Point(590, 287);
+            this.pwTextBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.pwTextBox.Name = "pwTextBox";
-            this.pwTextBox.Size = new System.Drawing.Size(222, 28);
+            this.pwTextBox.Size = new System.Drawing.Size(249, 32);
             this.pwTextBox.TabIndex = 4;
             this.pwTextBox.UseSystemPasswordChar = true;
             this.pwTextBox.TextChanged += new System.EventHandler(this.pwTextBox_TextChanged);
@@ -90,9 +96,10 @@
             this.signBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
             this.signBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.signBtn.ForeColor = System.Drawing.Color.White;
-            this.signBtn.Location = new System.Drawing.Point(313, 245);
+            this.signBtn.Location = new System.Drawing.Point(717, 409);
+            this.signBtn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.signBtn.Name = "signBtn";
-            this.signBtn.Size = new System.Drawing.Size(109, 47);
+            this.signBtn.Size = new System.Drawing.Size(123, 59);
             this.signBtn.TabIndex = 5;
             this.signBtn.Text = "Sign up";
             this.signBtn.UseVisualStyleBackColor = false;
@@ -101,20 +108,50 @@
             // pwShowCheckBox
             // 
             this.pwShowCheckBox.AutoSize = true;
-            this.pwShowCheckBox.Location = new System.Drawing.Point(200, 221);
+            this.pwShowCheckBox.Location = new System.Drawing.Point(590, 329);
+            this.pwShowCheckBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.pwShowCheckBox.Name = "pwShowCheckBox";
-            this.pwShowCheckBox.Size = new System.Drawing.Size(62, 20);
+            this.pwShowCheckBox.Size = new System.Drawing.Size(75, 24);
             this.pwShowCheckBox.TabIndex = 6;
             this.pwShowCheckBox.Text = "Show";
             this.pwShowCheckBox.UseVisualStyleBackColor = true;
             this.pwShowCheckBox.CheckedChanged += new System.EventHandler(this.pwShowCheckBox_CheckedChanged);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(27, 13);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(390, 532);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            // 
+            // cancelBtn
+            // 
+            this.cancelBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.cancelBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cancelBtn.ForeColor = System.Drawing.Color.White;
+            this.cancelBtn.Location = new System.Drawing.Point(483, 409);
+            this.cancelBtn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.cancelBtn.Name = "cancelBtn";
+            this.cancelBtn.Size = new System.Drawing.Size(123, 59);
+            this.cancelBtn.TabIndex = 17;
+            this.cancelBtn.Text = "Cancel";
+            this.cancelBtn.UseVisualStyleBackColor = false;
+            this.cancelBtn.Click += new System.EventHandler(this.cancelBtn_Click);
+            // 
             // signUpFrm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(900, 562);
             this.ControlBox = false;
+            this.Controls.Add(this.cancelBtn);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.pwShowCheckBox);
             this.Controls.Add(this.signBtn);
             this.Controls.Add(this.pwTextBox);
@@ -123,8 +160,10 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "signUpFrm";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -139,5 +178,7 @@
         private System.Windows.Forms.TextBox pwTextBox;
         private System.Windows.Forms.Button signBtn;
         private System.Windows.Forms.CheckBox pwShowCheckBox;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button cancelBtn;
     }
 }
